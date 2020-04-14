edbbbef04d44a7c61679f93d47db3187a14f3d34
Brooke Claroni <brookeclaroni@Brookes-MacBook-Pro.local>
2020-04-14 04:22:20 -0400

    phase 2 working

--- DCConcerts/app/src/main/java/com/example/dcconcerts/Result.kt
+++ DCConcerts/app/src/main/java/com/example/dcconcerts/Result.kt
@@ -4,7 +4,7 @@ data class Result(
         val event: String,
         val artist: String,
         val date: String,
-        val song1: String,
-        val song2: String,
-        val song3: String
+        val song1: String?,
+        val song2: String?,
+        val song3: String?
 )
